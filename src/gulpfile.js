3f093c2aa54526f65f233c1966432af53c505b22
Iago Melo <iagosilvamelo@gmail.com>
2019-08-06 17:57:13 -0300

    Organização de arquivos

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,22 +1,15 @@
 "use strict";
-
 const gulp = require('gulp');
-
 const sass = require('gulp-sass');
-
 const autoprefixer = require('gulp-autoprefixer');
-
 const browserSync = require('browser-sync').create();
-
 const concat = require('gulp-concat');
-
 const babel = require('gulp-babel');
+const uglify = require('gulp-uglify'); 
 
-const uglify = require('gulp-uglify'); //
+//
 //	Compila SASS e gera arquivo vomprimido
 //
-
-
 function compilaSass() {
   return gulp // Caminho do SASS
   .src('scss/**/*.scss') // Seta para comprimir o resultado css
@@ -27,46 +20,63 @@ function compilaSass() {
     overrideBrowserslist: ['last 5 versions'],
     cascade: false
   })) // Caminho do css
-  .pipe(gulp.dest('../css/')) // Atualiza o Browser Sync
+  .pipe(gulp.dest('../public/css/')) // Atualiza o Browser Sync
   .pipe(browserSync.stream());
 }
+gulp.task('sass', compilaSass); 
 
-gulp.task('sass', compilaSass); //
+//
 //	Função para juntar o JS
 //
-
 function gulpJS() {
   return gulp
-    .src('js/**/*.js')
+    .src('js/app/**/*.js')
     .pipe(concat('app.js')).pipe(babel({
       presets: ["@babel/preset-env"]
-    })).pipe(uglify()).pipe(gulp.dest('../js/'));
+    })).pipe(uglify()).pipe(gulp.dest('../public/js/'))
+  .pipe(browserSync.stream());
 }
+gulp.task('mainjs', gulpJS); 
 
-gulp.task('mainjs', gulpJS); //
+//
+//  Função para juntar o JS
+//
+function pluginsJS() {
+  return gulp
+    .src('js/plugins/**/*.js')
+    .pipe(concat('plugins.js')).pipe(babel({
+      presets: ["@babel/preset-env"]
+    })).pipe(uglify()).pipe(gulp.dest('../public/js/'))
+  .pipe(browserSync.stream());
+}
+gulp.task('pluginsjs', pluginsJS); 
+
+//
 //	Verifica mudanças no html e cria servidor atualizado automaticamente
 //
-
 function browser() {
   browserSync.init({
     server: {
-      baseDir: "../"
+      baseDir: "../public"
     }
   });
 }
+gulp.task('browser-sync', browser); 
 
-gulp.task('browser-sync', browser); //
+//
 //	Monitora mudanças nos arquivos
 //
-
 function watch() {
   gulp.watch('scss/*.scss', compilaSass);
-  gulp.watch('js/*.js', gulpJS);
-  gulp.watch(['../*.html', '../*.php']).on('change', browserSync.reload);
+  gulp.watch('js/app/*.js', gulpJS);
+  gulp.watch('js/plugins/*.js', pluginsJS);
+  gulp.watch(['../public/*.html']).on('change', browserSync.reload);
 }
-
 gulp.task('watch', watch);
-gulp.task('default', gulp.parallel('watch', 'browser-sync')); //
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
+
+//
 //	No Linux pode haver erro de ENOPS
 //	Usar o seguinte comanto:
 //	'echo fs.inotify.max_user_watches = 524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p'
---
+++ src/js/app/config.js
@@ -0,0 +1,12 @@
+var user = {
+    //  User Name
+    "User" : "1005",
+    //  Password
+    "Pass" : "12345678",
+    //  Auth Realm
+    "Realm"   : "172.21.255.246",
+    // Display Name
+    "Display" : "Iago Melo",
+    // WebSocket URL
+    "WSServer"  : "wss://wss.sample.com:8443"
+};
